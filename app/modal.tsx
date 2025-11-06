bc675b01a573399f426c4def97227e4c15246c0c
Nagajusem <notro123456@gmail.com>
2025-11-07 00:20:37 +0900

    ver4

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,8 +1,9 @@
 import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from '@/components/ui/themed-text';
+import { ThemedView } from '@/components/ui/themed-view';
+import React from 'react';
 
 export default function ModalScreen() {
   return (
